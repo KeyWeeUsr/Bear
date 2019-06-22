44f2eabc7c8fce77b321f6864a32fed6e213eadf
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-06-23 01:10:44 +0200

    [FIX] Fix test when running against flat file list

--- bear/tests/test_main.py
+++ bear/tests/test_main.py
@@ -29,13 +29,15 @@ class MainCase(TestCase):
         target = 'testpath'
         sysv = patch('sys.argv', [__name__, '--hash', target])
         hfile = patch('bear.__main__.hash_files')
-        ffile = patch('bear.__main__.find_files')
+        ffile_ret = [None]
+        ffile = patch('bear.__main__.find_files', return_value=ffile_ret)
         fhash = patch('bear.__main__.filter_files')
 
         with sysv, hfile as m_hash, ffile as m_find, fhash as m_filter:
             run()
             m_find.assert_called_once_with(target)
             m_hash.assert_called_once_with(m_find.return_value)
+            self.assertEqual(m_find.return_value, ffile_ret)
             m_filter.assert_called_once_with(m_hash.return_value)
 
 
