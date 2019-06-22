a3cde1641e786cfa3d4f6b6ac89f4eff66983058
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-06-22 22:22:17 +0200

    [ADD] Add getting MD5 hash for a file input

--- bear/__init__.py
+++ bear/__init__.py
@@ -5,5 +5,17 @@ Main file for the Bear package.
 from hashlib import md5
 
 
-def hasher(inp):
+def hash_text(inp):
+    """
+    Hash simple string of text.
+    """
     return md5(inp).hexdigest()
+
+
+def hash_file(path):
+    """
+    Open a file, read its contents and return MD5 hash.
+    """
+    with open(path, 'rb') as file:
+        contents = file.read()
+    return hash_text(contents)
--- bear/tests/test_hash.py
+++ bear/tests/test_hash.py
@@ -3,16 +3,26 @@ Test hashing of specific file contents.
 """
 
 from unittest import TestCase, main
-
-from bear import hasher
+from tempfile import mkstemp
+from os import remove
+from bear import hash_text, hash_file
 
 
 class HashCase(TestCase):
     def test_hash_word(self):
         self.assertEqual(
             '098f6bcd4621d373cade4e832627b4f6',
-            hasher('test'.encode('utf-8'))
+            hash_text('test'.encode('utf-8'))
         )
+
+    def test_hash_file(self):
+        base = b'\xecO\xda\xad\x12\x85\xa1\xe1\xbd\xb7\xf1'
+        (desc, path) = mkstemp(text=False)
+        with open(desc, 'wb') as file:
+            file.write(base)
+        hashed = hash_file(path)
+        remove(path)
+        self.assertEqual('7972892f41d1b98a71d9583b83267d8b', hashed)
 
 
 if __name__ == '__main__':
