d47c50f4c373e281313f57278d512d463ff4731c
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-06-22 23:51:48 +0200

    [ADD] Add test for --hash parameter

--- bear/tests/test_main.py
+++ bear/tests/test_main.py
@@ -19,11 +19,21 @@ class MainCase(TestCase):
 
     def test_find_files(self):
         target = 'testpath'
-        hash_file = patch('bear.__main__.find_files')
+        find_file = patch('bear.__main__.find_files')
         sysargv = patch('sys.argv', [__name__, '--traverse', target])
-        with sysargv, hash_file as mocked:
+        with sysargv, find_file as mocked:
             run()
             mocked.assert_called_once_with(target)
+
+    def test_hash_folder(self):
+        target = 'testpath'
+        hash_file = patch('bear.__main__.hash_files')
+        find_file = patch('bear.__main__.find_files')
+        sysargv = patch('sys.argv', [__name__, '--hash', target])
+        with sysargv, hash_file as mocked_hash, find_file as mocked_find:
+            run()
+            mocked_find.assert_called_once_with(target)
+            mocked_hash.assert_called_once_with(mocked_find.return_value)
 
 
 if __name__ == '__main__':
