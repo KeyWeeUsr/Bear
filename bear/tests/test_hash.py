9bd7ce2e735f46a6a6fe5d19e3a8839d39dbfe5d
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-07-02 00:13:28 +0200

    [FIX] Fix test for hashing functions, use kwargs

--- bear/tests/test_hash.py
+++ bear/tests/test_hash.py
@@ -5,6 +5,7 @@ Test hashing of specific file contents.
 from unittest import TestCase, main
 from tempfile import mkstemp
 from os import remove
+from bear.common import Hasher
 from bear.hashing import hash_text, hash_file
 
 
@@ -13,16 +14,16 @@ class HashCase(TestCase):
     Test hashing functions.
     """
 
-    def test_hash_word(self):
+    def test_hash_word_md5(self):
         """
         Test hashing a string.
         """
         self.assertEqual(
             '098f6bcd4621d373cade4e832627b4f6',
-            hash_text('test'.encode('utf-8'))
+            hash_text(inp='test'.encode('utf-8'), hasher=Hasher.MD5)
         )
 
-    def test_hash_file(self):
+    def test_hash_file_md5(self):
         """
         Test hashing bytes of a file.
         """
@@ -30,7 +31,7 @@ class HashCase(TestCase):
         (desc, path) = mkstemp(text=False)
         with open(desc, 'wb') as file:
             file.write(base)
-        hashed = hash_file(path)
+        hashed = hash_file(path=path, hasher=Hasher.MD5)
         remove(path)
         self.assertEqual('7972892f41d1b98a71d9583b83267d8b', hashed)
 
