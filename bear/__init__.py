9eecc75c010a6dc00438cf357c7004fed1e1e462
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-09-11 21:56:12 +0200

    [ADD] Bump version to 0.5.0

--- bear/__init__.py
+++ bear/__init__.py
@@ -5,7 +5,7 @@ Main file for the Bear package.
 NAME = 'bear'
 PYPI_NAME = 'thebear'
 PYPI_INDEX = 'https://pypi.org/simple'
-VERSION = '0.4.1'
+VERSION = '0.5.0'
 GITHUB_WEB = 'https://github.com'
 GITHUB_API = 'https://api.github.com'
 REPO_PATH = 'KeyWeeUsr/Bear'
