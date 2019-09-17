804a11f8d52bd0685db0929da3ab5294732644ce
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2019-09-17 21:00:12 +0200

    [ADD] Add tests for SHA256 and BLAKE2 hash modes

--- bear/tests/test_hash.py
+++ bear/tests/test_hash.py
@@ -23,6 +23,24 @@ class HashCase(TestCase):
             hash_text(inp='test'.encode('utf-8'), hasher=Hasher.MD5)
         )
 
+    def test_hash_word_sha256(self):
+        """
+        Test hashing a string.
+        """
+        self.assertEqual(
+            "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
+            hash_text(inp='test'.encode('utf-8'), hasher=Hasher.SHA256)
+        )
+
+    def test_hash_word_blake2(self):
+        """
+        Test hashing a string.
+        """
+        self.assertEqual((
+            "a71079d42853dea26e453004338670a53814b78137ffbed07603a41d76a483aa"
+            "9bc33b582f77d30a65e6f29a896c0411f38312e1d66e0bf16386c86a89bea572"
+        ), hash_text(inp='test'.encode('utf-8'), hasher=Hasher.BLAKE2))
+
     def test_hash_file_md5(self):
         """
         Test hashing bytes of a file.
